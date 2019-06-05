ae5362dcecf44700e6ece99d1544c6e59b4a6814
eldenhowes <williameldenhowes@gmail.com>
2019-06-05 19:08:37 +0100

    Refactor

--- group_array_of_hashes.rb
+++ group_array_of_hashes.rb
@@ -7,29 +7,12 @@ players = [
 sorted_by_sport = Hash.new
 names_array = Array.new
 
-players.each do |player_under_consideration|
-  sport = player_under_consideration[:sport]
-  name = player_under_consideration[:name]
+players.each do |player|
+  sport = player[:sport]
 
   if sorted_by_sport[sport] == nil
-    sorted_by_sport[sport] = [name]
-  else
-    sorted_by_sport[sport].push(name)
+    sorted_by_sport[sport] = []
   end
+  sorted_by_sport[sport].push(player[:name])
 end
 puts sorted_by_sport
-
-
-
-# OLD VERSION"
-
-# if sorted_by_sport.include?(player_under_consideration[:sport])
-#   # add the name to the array
-#   names_array.push(player_under_consideration[:name])
-#   # add the previous name that plays this sport to array
-#   names_array.push(sorted_by_sport[player_under_consideration[:sport]])
-#   # add names_array to sorted_by_sport Hash
-#   sorted_by_sport[player_under_consideration[:sport]] = names_array
-# else
-#   sorted_by_sport[player_under_consideration[:sport]] = player_under_consideration[:name]
-# end
