951fa551fa938d9bab23afc523182d7e0fa4833f
eldenhowes <williameldenhowes@gmail.com>
2019-06-05 19:01:56 +0100

    Better version

--- group_array_of_hashes.rb
+++ group_array_of_hashes.rb
@@ -8,16 +8,28 @@ sorted_by_sport = Hash.new
 names_array = Array.new
 
 players.each do |player_under_consideration|
-  if sorted_by_sport.include?(player_under_consideration[:sport])
-    # add the name to the array
-    names_array.push(player_under_consideration[:name])
-    # add the previous name that plays this sport to array
-    names_array.push(sorted_by_sport[player_under_consideration[:sport]])
-    # add names_array to sorted_by_sport Hash
-    sorted_by_sport[player_under_consideration[:sport]] = names_array
-  else
-    sorted_by_sport[player_under_consideration[:sport]] = player_under_consideration[:name]
-  end
+  sport = player_under_consideration[:sport]
+  name = player_under_consideration[:name]
 
+  if sorted_by_sport[sport] == nil
+    sorted_by_sport[sport] = [name]
+  else
+    sorted_by_sport[sport].push(name)
+  end
 end
 puts sorted_by_sport
+
+
+
+# OLD VERSION"
+
+# if sorted_by_sport.include?(player_under_consideration[:sport])
+#   # add the name to the array
+#   names_array.push(player_under_consideration[:name])
+#   # add the previous name that plays this sport to array
+#   names_array.push(sorted_by_sport[player_under_consideration[:sport]])
+#   # add names_array to sorted_by_sport Hash
+#   sorted_by_sport[player_under_consideration[:sport]] = names_array
+# else
+#   sorted_by_sport[player_under_consideration[:sport]] = player_under_consideration[:name]
+# end
