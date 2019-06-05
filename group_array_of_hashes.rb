04899c13c078ab765260f5399a27223d9320dde5
eldenhowes <williameldenhowes@gmail.com>
2019-06-05 18:51:22 +0100

    Made program work but could be improved

--- group_array_of_hashes.rb
+++ group_array_of_hashes.rb
@@ -4,9 +4,21 @@ players = [
   { :name => "Ed", :sport => "tennis" },
   { :name => "Mark", :sport => "football" }
   ]
-sports_hash = Hash.new
+sorted_by_sport = Hash.new
+names_array = Array.new
 
 players.each do |hash_in_array|
-  sports_hash[hash_in_array[:sport]] = hash_in_array[:name]
+  if sorted_by_sport.include?(hash_in_array[:sport])
+    # add the name to the array
+    names_array.push(hash_in_array[:name])
+    # add the previous name that plays this sport to array
+    names_array.push(sorted_by_sport[hash_in_array[:sport]])
+    # add names_array to sorted_by_sport Hash
+    sorted_by_sport[hash_in_array[:sport]] = names_array
+  else
+    sorted_by_sport[hash_in_array[:sport]] = hash_in_array[:name]
+  end
+
 end
-puts sports_hash
+puts names_array
+puts sorted_by_sport
